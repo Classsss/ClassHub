db8d1c65fdb2eb9ad7d65746094f8031fcc242a6
byungmeo <wlsrlfvkr@gmail.com>
2023-03-20 23:15:12 +0900

    Create Sample Project

---
+++ ClassHub/Client/Program.cs
@@ -0,0 +1,17 @@
+using ClassHub.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace ClassHub.Client {
+    public class Program {
+        public static async Task Main(string[] args) {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
