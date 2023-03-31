c9015bed92b7c3acfd026dec2b7ed10b3d71f4bc
byungmeo <wlsrlfvkr@gmail.com>
2023-03-31 21:41:05 +0900

    강의 모델 생성

---
+++ Client/Models/Course.cs
@@ -0,0 +1,10 @@
+﻿namespace ClassHub.Client.Models {
+    public class Course {
+        public string Name { get; set; }
+        public string Instructor { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string ClassRoom { get; set; }
+    }
+
+}
