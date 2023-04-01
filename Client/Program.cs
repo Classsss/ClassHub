dd99e1e5027db8793d1d07fdacbd8dffe4673f4c
Byungmeo <wlsrlfvkr@gmail.com>
2023-04-02 05:08:11 +0900

    로그인 정보를 저장하는 싱글턴 서비스 클래스 생성 및 등록

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,4 +1,5 @@
 using ClassHub.Client;
+using ClassHub.Client.Shared;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -10,6 +11,7 @@ namespace ClassHub.Client {
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddSingleton<AuthenticationService>();
 
             await builder.Build().RunAsync();
         }
