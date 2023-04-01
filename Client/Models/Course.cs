a4c74c451ebf632e199a7c6051652c98e4996ad5
Byungmeo <wlsrlfvkr@gmail.com>
2023-04-02 06:25:45 +0900

    강의 일자를 추가하고 시작 시간과 종료 시간을 정수형으로 변경

--- Client/Models/Course.cs
+++ Client/Models/Course.cs
@@ -2,8 +2,9 @@
     public class Course {
         public string Name { get; set; }
         public string Instructor { get; set; }
-        public DateTime StartTime { get; set; }
-        public DateTime EndTime { get; set; }
+        public DayOfWeek Day { get; set; }
+        public int StartTime { get; set; }
+        public int EndTime { get; set; }
         public string ClassRoom { get; set; }
     }
 
