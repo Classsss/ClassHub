fe6f92feb12ca10f93c3c6587c80d1ac2d464681
byungmeo <wlsrlfvkr@gmail.com>
2023-04-02 23:38:28 +0900

    콘솔만 뜨도록 수정

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,4 +1,5 @@
 using ClassHub.Client;
+using ClassHub.Client.Shared;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -11,6 +12,7 @@ namespace ClassHub.Client {
 
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddSingleton<AuthenticationService>();
 
             await builder.Build().RunAsync();
         }
