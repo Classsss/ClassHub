a69815236a5a30285ea75d1d5e9120a6dce950c1
byungmeo <wlsrlfvkr@gmail.com>
2023-04-03 02:27:33 +0900

    Announcement 관련 클래스 이름 변경

--- Client/Models/Announcement.cs
+++ Client/Models/Announcement.cs
@@ -1,5 +1,5 @@
 ﻿namespace ClassHub.Client.Models {
-    public class _Announcement {
+    public class Announcement {
         public int Id { get; set; } // 공지사항 번호
         public string Title { get; set; } // 공지사항 제목
         public string Content { get; set; } // 공지사항 내용
@@ -10,6 +10,5 @@
         // public List<string> Attachments { get; set; } // 첨부 파일 정보
         // public string Category { get; set; } // 분류
         // public bool IsImportant { get; set; } // 중요 여부
-
     }
 }
