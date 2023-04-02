1e1a5626e8427ddb83b7e8ed8b595e9c529e83d7
byungmeo <wlsrlfvkr@gmail.com>
2023-04-02 09:23:05 +0900

    강의 구분을 위한 모델 속성 추가 (강좌번호, 연도, 학기)

--- Client/Models/Course.cs
+++ Client/Models/Course.cs
@@ -1,11 +1,14 @@
 ﻿namespace ClassHub.Client.Models {
     public class Course {
-        public string Name { get; set; }
-        public string Instructor { get; set; }
-        public DayOfWeek Day { get; set; }
-        public int StartTime { get; set; }
-        public int EndTime { get; set; }
-        public string ClassRoom { get; set; }
+        public int Number { get; set; } // 강좌 번호
+        public int Year { get; set; } // 개설 년도
+        public int Semester { get; set; } // 개설 학기
+        public string Name { get; set; } // 교과목명
+        public string Instructor { get; set; } // 담당교수
+        public DayOfWeek Day { get; set; } // 강의요일
+        public int StartTime { get; set; } // 강의 시작시간
+        public int EndTime { get; set; } // 강의 종료시간
+        public string ClassRoom { get; set; } // 강의실
     }
 
 }
