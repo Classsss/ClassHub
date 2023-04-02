94bd9e15eaaf4fd9d13993b1b2f0104ac57bb225
you990917 <you990917@naver.com>
2023-04-02 23:12:51 +0900

    간단한 공지사항 리스트 구현

--- Client/Program.cs
+++ Client/Program.cs
@@ -9,6 +9,7 @@ namespace ClassHub.Client {
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
+
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
