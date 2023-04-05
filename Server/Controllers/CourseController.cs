4566865488e933f8df3edfda866cab4fb745fcb6
sch990428 <990428sch@naver.com>
2023-04-05 11:21:03 +0900

    학사정보 시스템에서의 수강과목 API 활용

--- Server/Controllers/CourseController.cs
+++ Server/Controllers/CourseController.cs
@@ -17,29 +17,15 @@ namespace ClassHub.Server.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> getCourses()
+        public async Task<IActionResult> POST()
         {
-            /*
-            나중에 토큰의 만료기간, 유효성을 검사하는 부분이 필요하다고 판단되면 요청을 추가할것
+            //앱 서버로 수강과목 출력을 요청
             var client = new HttpClient();
-            var tokenRequest = new HttpRequestMessage(HttpMethod.Post, "https://classhubsso.azurewebsites.net/token");
-
-            var authCode = request.AuthorizationCode;
-            tokenRequest.Content = new StringContent(JsonSerializer.Serialize(new AccessTokenRequest { AuthorizationCode = authCode }), Encoding.UTF8, "application/json");
-                    
-            var response = await client.SendAsync(tokenRequest);
+            var request = new HttpRequestMessage(HttpMethod.Post, "https://academicinfo.azurewebsites.net/Course");
+            var response = await client.SendAsync(request);
             var content = await response.Content.ReadAsStringAsync();
-            */
-            Console.WriteLine("학사정보 서비스로 요청하기");
-            var connectionString = "Host=\r\nacademic-info-db.postgres.database.azure.com\r\n;Username=classhub;Password=ch55361!;Database=AcademicInfo";
-            var connection = new NpgsqlConnection(connectionString);
 
-            var query = "SELECT * FROM course";
-            var results = connection.Query<CourseResponse>(query);
-
-            connection.Dispose();
-
-            return Ok(results);
+            return Ok(content);
         }
     }
 }
