55bf0db435321b6a8936867d4b942bc10eff30a9
byungmeo <wlsrlfvkr@gmail.com>
2023-04-07 05:40:51 +0900

    강의실 모델 생성

---
+++ Shared/Classroom.cs
@@ -0,0 +1,10 @@
+﻿namespace ClassHub.Client.Models {
+    public class Classroom {
+        public int Id { get; set; }
+        public string CourseId { get; set; } // ABC12345
+        public int SectionId { get; set; } // 0810
+        public string Semester { get; set; } // SPRING, SUMMER, FALL, WINTER         
+        public int Year { get; set; } // 2023
+        public string Title { get; set; } // 캡스톤디자인
+    }
+}
