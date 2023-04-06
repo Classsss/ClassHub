eeb4d68bd0026e0466720bb2dbb2f01c8af38c82
byungmeo <wlsrlfvkr@gmail.com>
2023-04-07 07:50:57 +0900

    ResizeEditor.js에서 발생하는 오류에 대해 try-catch 적용

--- Client/wwwroot/ResizeEditor.js
+++ Client/wwwroot/ResizeEditor.js
@@ -7,11 +7,14 @@ window.onresize = function () {
 };
 
 function resizeEditor() {
-    var editorElement = document.getElementById("editor");
-    var style = window.getComputedStyle(editorElement);
-    var height = window.innerHeight - 300 - parseFloat(style.borderTopWidth) - parseFloat(style.borderBottomWidth) - parseFloat(style.paddingTop) - parseFloat(style.paddingBottom);
-    var width = window.innerWidth - 300 - parseFloat(style.borderLeftWidth) - parseFloat(style.borderRightWidth) - parseFloat(style.paddingLeft) - parseFloat(style.paddingRight);
-    editorElement.style.height = height + "px";
-    editorElement.style.width = width + "px";
+    try {
+        var editorElement = document.getElementById("editor");
+        var style = window.getComputedStyle(editorElement);
+        var height = window.innerHeight - 300 - parseFloat(style.borderTopWidth) - parseFloat(style.borderBottomWidth) - parseFloat(style.paddingTop) - parseFloat(style.paddingBottom);
+        var width = window.innerWidth - 300 - parseFloat(style.borderLeftWidth) - parseFloat(style.borderRightWidth) - parseFloat(style.paddingLeft) - parseFloat(style.paddingRight);
+        editorElement.style.height = height + "px";
+        editorElement.style.width = width + "px";
+    } catch (e) {
 
+    }
 }
