12d066c91983c1720a311a253f5a7f3bca9e8515
you990917 <you990917@naver.com>
2023-04-08 04:18:24 +0900

    AnnouncementList UI 제작, 각종 기능 추가, 현재 페이지네이션 저장 안되는 문제점 있음

--- Client/Models/Announcement.cs
+++ Client/Models/Announcement.cs
@@ -4,11 +4,6 @@
         public string Title { get; set; } // 공지사항 제목
         public string Content { get; set; } // 공지사항 내용
         public string Author { get; set; } // 작성자
-        public DateTime StartDate { get; set; } // 시작일
-        // public DateTime EndDate { get; set; } // 종료일
-        // public int Priority { get; set; } // 우선순위
-        // public List<string> Attachments { get; set; } // 첨부 파일 정보
-        // public string Category { get; set; } // 분류
-        // public bool IsImportant { get; set; } // 중요 여부
+        public DateTime StartDate { get; set; } // 게시일
     }
 }
