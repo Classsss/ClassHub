d2b9fef16f87ce459ea375cf4d1a8404b60a28ed
byungmeo <wlsrlfvkr@gmail.com>
2023-04-13 07:04:20 +0900

    시험문제 모델 배점속성 추가

--- Client/Models/ExamProblem.cs
+++ Client/Models/ExamProblem.cs
@@ -1,23 +1,24 @@
 ﻿namespace ClassHub.Client.Models {
-public class ExamProblem {
-    public int ProblemId { get; set; }
-    public string Description { get; set; }
-}
+    public class ExamProblem {
+        public int ProblemId { get; set; }
+        public string Description { get; set; }
+        public int Score { get; set; }
+    }
 
-// 객관식
-public class MultipleChoiceProblem : ExamProblem {
-    public string[] Questions { get; set; }
-        public int Answer { get; set; } = -1;
-}
+    // 객관식
+    public class MultipleChoiceProblem : ExamProblem {
+        public string[] Questions { get; set; }
+            public int Answer { get; set; } = -1;
+    }
 
-// 단답형
-public class ShortAnswerProblem : ExamProblem {
-    public string Answer { get; set; } = "";
-}
+    // 단답형
+    public class ShortAnswerProblem : ExamProblem {
+        public string Answer { get; set; } = "";
+    }
 
-// 코드형
-public class CodeProblem : ExamProblem {
-    public string Example { get; set; } = "";
-    public string Answer { get; set; } = "";
-}
+    // 코드형
+    public class CodeProblem : ExamProblem {
+        public string Example { get; set; } = "";
+        public string Answer { get; set; } = "";
+    }
 }
