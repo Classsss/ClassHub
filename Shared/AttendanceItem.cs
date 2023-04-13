d676a67ce55d60b00b470ddec99f97edced4f899
sch990428 <990428sch@naver.com>
2023-04-13 14:56:36 +0900

    AttendanceStatus 컴포넌트 대거 수정 기존 컴포넌트는 Unused로 이동

---
+++ Shared/AttendanceItem.cs
@@ -0,0 +1,11 @@
+﻿namespace ClassHub.Shared {
+    public class AttendanceItem {
+        public int Week { get; set; }
+        public int Lesson { get; set; }
+        public string Title { get; set; } //학습 제목
+        public string LearningType { get; set; } // 학습유형
+        public string LearningProgress { get; set; } // 학습현황
+        public string AttendProgress { get; set; } // 출결현황
+        public string DetailLink { get; set; } // 대상 자료 링크
+    }
+}
