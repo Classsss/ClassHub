dff9ec0589f66a8eb58079b9b34e6422ecfa14b4
byungmeo <wlsrlfvkr@gmail.com>
2023-04-17 08:45:17 +0900

    시간표에서 강의를 클릭하여 강의실 진입 시 네비게이션 메뉴 타이틀이 강의명으로 변경되도록 수정

--- Client/Program.cs
+++ Client/Program.cs
@@ -13,6 +13,7 @@ namespace ClassHub.Client {
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddSingleton<AuthenticationService>();
+            builder.Services.AddSingleton<NavMenuTitleService>();
             builder.Services.AddBlazoredModal();
 
 
