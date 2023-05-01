6579e9d74c6b727b707ac6ba32f5eac77e15a057
MadeByZiNo <38098073+MadeByZiNo@users.noreply.github.com>
2023-05-01 23:56:20 +0900

    교수 코드로 output 비교 수정에 맞게 request 수정

--- Shared/JudgeRequest.cs
+++ Shared/JudgeRequest.cs
@@ -3,7 +3,8 @@
     public class JudgeRequest {
         public string CorrectCode { get; set; }           // 교수가 제출한 정답 코드
         public string SubmitCode { get; set; }            // 학생이 에디터에 작성하여 제출한 코드
-        public string Language { get; set; }              // 코드의 프로그래밍 언어
+        public string CorrectCodeLanguage { get; set; }   // 정답 코드의 언어
+        public string SubmitCodeLanguage { get; set; }    // 학생 코드의 언어
         public List<string> InputCases { get; set; }      // 입력 테스트 케이스
         public double ExecutionTimeLimit { get; set; }    // 실행 시간 제한
         public long MemoryUsageLimit { get; set; }        // 메모리 사용량 제한
