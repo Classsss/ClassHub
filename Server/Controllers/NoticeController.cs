8b02dbb864833ac07317089a4e5546f18c4d6384
you990917 <you990917@naver.com>
2023-05-03 19:36:08 +0900

    임시

---
+++ Server/Controllers/NoticeController.cs
@@ -0,0 +1,29 @@
+﻿using ClassHub.Shared;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+
+namespace ClassHub.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NoticeController : ControllerBase
+    {
+        const string host = "classdb.postgres.database.azure.com";
+        const string username = "byungmeo";
+        const string passwd = "Mju12345!#";
+        const string database = "classdb";
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<Notice>>> Get(int id)
+        {
+            var connectionString = $"Host={host};Username={username};Password={passwd};Database={database}";
+            using var connection = new NpgsqlConnection(connectionString);
+
+            var query = $"SELECT * FROM Notice WHERE room_id = {id}";
+            var notices = await connection.QueryAsync<Notice>(query);
+            return notices.ToList();
+        }
+
+    }
+}
