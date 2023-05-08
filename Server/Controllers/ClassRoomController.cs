585f1ae77c080803ca5120752f0d01bd4ded2df9
byungmeo <wlsrlfvkr@gmail.com>
2023-05-08 13:36:01 +0900

    SQL Injection 방지를 위해 ClassRoomController 수정

--- Server/Controllers/ClassRoomController.cs
+++ Server/Controllers/ClassRoomController.cs
@@ -13,39 +13,41 @@ namespace ClassHub.Server.Controllers {
         const string database = "classdb";
         const string connectionString = $"Host={host};Username={username};Password={passwd};Database={database}";
 
-        // 실제 요청 url 예시 : 'api/classroom/1' <- 1번 강의실의 정보를 불러옴
+        // Param으로 받은 ID를 가진 강의실의 정보를 불러옴
+        // 실제 요청 url 예시 : 'api/classroom/1'
         [HttpGet("{room_id}")]
-        public ClassRoom GetClassRoom(int room_id) {
-            var connection = new NpgsqlConnection(connectionString);
-
-            var query = $"SELECT * FROM classroom WHERE \"room_id\" = {room_id};"; // 강의실 번호가 id인 강의실을 찾습니다.
-            var result = connection.Query<ClassRoom>(query).FirstOrDefault(); // ID는 고유하므로, 하나만 반환되는 것이 자명하여 FirstOrDefault()를 통해 첫 번째 요소를 반환합니다. (없으면 기본값)
-
-            connection.Dispose();
-
+        public ClassRoom? GetClassRoom(int room_id) {
+            using var connection = new NpgsqlConnection(connectionString);
+            var query = 
+                "SELECT * " +
+                "FROM classroom " +
+                "WHERE room_id = @room_id;";
+            var result = connection.Query<ClassRoom>(query, room_id).FirstOrDefault(); // ID는 고유하므로, 하나만 반환되는 것이 자명하여 FirstOrDefault()를 통해 첫 번째 요소를 반환합니다. (없으면 기본값)
             return result;
         }
 
         // 실제 요청 url 예시 : 'api/classroom/1/lecturematerial/all' <- 1번 강의실의 모든 강의자료를 불러옴
         [HttpGet("lecturematerial/all/{room_id}")]
         public IEnumerable<LectureMaterial> GetAllLectureMaterialsInClassRoom(int room_id) {
-            Console.WriteLine($"room id : {room_id}");
             using var connection = new NpgsqlConnection(connectionString);
-            string query = $"SELECT * FROM lecturematerial WHERE \"room_id\" = {room_id};"; // room_id가 동일한 모든 공지사항을 찾습니다.
-            var result = connection.Query<LectureMaterial>(query);
+            string query = 
+                "SELECT * " +
+                "FROM lecturematerial " +
+                "WHERE room_id = @room_id;";
+            var result = connection.Query<LectureMaterial>(query, room_id);
             return result;
         }
 
-        // 실제 요청 url 예시 : 'api/classroom/notification/all/60182147' <- 학번이 60182147인 학생에게 온 모든 알림을 불러옴
+        // Param으로 받은 학번을 가진 학생에게 온 모든 알림을 불러옴 (모든 수강 강의)
+        // 실제 요청 url 예시 : 'api/classroom/notification/all/60182147'
         [HttpGet("notification/all/{student_id}")]
         public IEnumerable<StudentNotification> GetAllNotifications(int student_id) {
-            var connection = new NpgsqlConnection(connectionString);
-
-            var query = $"SELECT * FROM studentnotification WHERE \"student_id\" = {student_id};"; // student_id가 동일한 모든 알림을 찾습니다.
-            var result = connection.Query<StudentNotification>(query);
-
-            connection.Dispose();
-
+            using var connection = new NpgsqlConnection(connectionString);
+            var query = 
+                "SELECT * " +
+                "FROM studentnotification " +
+                "WHERE student_id = @student_id;";
+            var result = connection.Query<StudentNotification>(query, student_id);
             return result;
         }
 
