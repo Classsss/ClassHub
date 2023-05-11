e1d9a1831309442ae2f2a4b078fcec1a493b30ff
MadeByZiNo <38098073+MadeByZiNo@users.noreply.github.com>
2023-05-11 16:34:10 +0900

    필요한 모델들 생성 및 수정

--- Shared/CodeProblem.cs
+++ Shared/CodeProblem.cs
@@ -5,7 +5,7 @@
         public int problem_id { get; set; }
         public string title { get; set; }
         public string author { get; set; }
-        public string content { get; set; } 
+        public string contents { get; set; } 
         public string standard_code { get; set; }
         public string standard_language { get; set; }
 
---
+++ Shared/CodeSubmit.cs
@@ -0,0 +1,18 @@
+﻿namespace ClassHub.Shared
+{
+    public class CodeSubmit
+    {
+        public int room_id { get; set; }
+        public int week { get; set; } 
+        public int assignment_id { get; set; } 
+        public int submit_id { get; set; }
+        public DateTime submit_date { get; set; }
+        public string status { get; set; }
+        public int student_id { get; set; }
+        public double exec_time { get; set; }          // 실행 시간(ms)    // TODO : 기준 정립 필요 (ex. 평균)
+        public long mem_usage { get; set; }              // 메모리 사용량(?) // TODO : 단위 지정 필요, 기준 정립 필요 (ex. 평균)
+        public string code { get; set; }
+        public string message { get; set; }
+        public string language { get; set; }
+    }
+}
