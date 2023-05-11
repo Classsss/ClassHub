88ac59e4e01b2997b45d3cc9a9fb630d93555486
Byungmeo <wlsrlfvkr@gmail.com>
2023-05-11 09:55:26 +0900

    새로운 알림 테이블 생성 및 변경점을 모델 클래스에 반영

---
+++ Shared/Notification.cs
@@ -0,0 +1,17 @@
+﻿namespace ClassHub.Shared {
+    public class ClassRoomNotification {
+        public int room_id { get; set; }
+        public int student_id { get; set; }
+        public int notification_id { get; set; }
+        public string message { get; set; }
+        public string uri { get; set; }
+        public DateTime notify_date { get; set; }
+    }
+
+    public class StudentNotification {
+        public int room_id { get; set; }
+        public int student_id { get; set; }
+        public int notification_id { get; set; }
+        public bool is_read { get; set; }
+    }
+}
