f9b330b09dde8b96bc87d2f2c5fce2e927269abc
MadeByZiNo <38098073+MadeByZiNo@users.noreply.github.com>
2023-05-13 15:06:01 +0900

    제출 여부를 위한 수정

--- Server/Controllers/PracticeListController.cs
+++ Server/Controllers/PracticeListController.cs
@@ -18,8 +18,8 @@ namespace ClassHub.Server.Controllers {
         //Todo: Submit과 연동해서 제출여부 확인, 
 
         // 해당 강의실에 올라온 실습 리스트를 보여준다.
-        [HttpGet("{room_id}")]
-        public List<Submission> GetPracticeList(int room_id) {
+        [HttpGet("room_id/{room_id}/student_id/{student_id}")]
+        public List<Submission> GetPracticeList(int room_id,int student_id) {
             using var connection = new NpgsqlConnection(connectionString);
             string query;
 
@@ -31,15 +31,23 @@ namespace ClassHub.Server.Controllers {
             var parameters = new DynamicParameters();
             parameters.Add("room_id", room_id);
             var codeAssignments = connection.Query<CodeAssignment>(query,parameters);
-            
+
             // 문제 번호가 problem_id인 문제를 찾은 후 practice를 생성하고 practiceList에 추가합니다.
-            foreach (CodeAssignment codeAssignment in codeAssignments)
-            {
+            foreach (CodeAssignment codeAssignment in codeAssignments) {
                 query = "SELECT * FROM codeproblem WHERE problem_id = @problem_id;";
                 var parametersProblem = new DynamicParameters();
                 parametersProblem.Add("problem_id", codeAssignment.problem_id);
-                Shared.CodeProblem codeProblem = connection.Query<Shared.CodeProblem>(query,parametersProblem).FirstOrDefault();
-                practiceList.Add(new Assignment {Id = codeAssignment.assignment_id, Title = codeProblem.title, Description = codeProblem.contents, Author = codeProblem.author, StartDate = codeAssignment.start_date, EndDate = codeAssignment.end_date, IsSubmitted = true });
+                Shared.CodeProblem codeProblem = connection.Query<Shared.CodeProblem>(query, parametersProblem).FirstOrDefault();
+
+                query = "SELECT COUNT(*) FROM codesubmit WHERE room_id = @room_id AND assignment_id = @assignment_id AND student_id = @student_id;";
+                var parametersSubmit = new DynamicParameters();
+                parametersSubmit.Add("room_id", codeAssignment.room_id);
+                parametersSubmit.Add("assignment_id", codeAssignment.assignment_id);
+                parametersSubmit.Add("student_id", student_id);
+                bool is_submit = true;
+                if (connection.QueryFirstOrDefault<int>(query, parametersSubmit) == 0) { is_submit = false; }
+
+                practiceList.Add(new Assignment {Id = codeAssignment.assignment_id, Title = codeProblem.title, Description = codeProblem.contents, Author = codeProblem.author, StartDate = codeAssignment.start_date, EndDate = codeAssignment.end_date, IsSubmitted = is_submit });
             }
             return practiceList;
         }
