a82dc70e03be3373e59a631fa6f916906d9582be
LeeHaYoon <htdobad@gmail.com>
2023-05-14 17:59:43 +0900

    채점을 요청하기 위해 JudgeRequest 객체와 CodeSubmit 객체를 함께 저장하는 클래스 추가

---
+++ Shared/RequestSubmitContainer.cs
@@ -0,0 +1,14 @@
+﻿namespace ClassHub.Shared {
+    // 채점을 요청하기 위해 JudgeRequest 객체와 CodeSubmit 객체를 함께 저장하는 클래스
+    public class RequestSubmitContainer {
+        // JudgeRequest 객체
+        public JudgeRequest? JudgeRequest { get; private set; }
+        // CodeSubmit 객체
+        public CodeSubmit? CodeSubmit {  get; private set; }
+
+        public RequestSubmitContainer(JudgeRequest judgeRequest, CodeSubmit codeSubmit) {
+            JudgeRequest = judgeRequest;
+            CodeSubmit = codeSubmit;
+        }
+    }
+}
