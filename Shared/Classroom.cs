388f202277366bae4c264176df7ca680222e820d
byungmeo <wlsrlfvkr@gmail.com>
2023-05-24 15:24:33 +0900

    강의실 세부정보 모델 생성

--- Shared/Classroom.cs
+++ Shared/Classroom.cs
@@ -7,4 +7,18 @@
         public int year { get; set; } // 2023
         public string title { get; set; } // 캡스톤디자인
     }
+
+    public class TimeSlot {
+        public DayOfWeek day { get; set; }
+        public int start_time { get; set; }
+        public int end_time { get; set; }
+    }
+
+    public class ClassRoomDetail : ClassRoom {
+        public List<TimeSlot> TimeSlots { get; set; }
+        public string Instructor { get; set; }
+        public string BuildingId { get; set; }
+
+        public int Color { get; set; } = -1;
+    }
 }
