c2851f4d976dc777fe8771932d8dc228e7d37ca2
LeeHaYoon <htdobad@gmail.com>
2023-06-03 01:45:55 +0900

    학생용 시험 리스트를 디비에서 받아와 제공하는 GET 메소드 추가 및 시험 리스트 페이지에서 학생의 경우에도 시험 리스트를 받아와 화면에 표시하도록 함

--- Server/Controllers/ExamListController.cs
+++ Server/Controllers/ExamListController.cs
@@ -14,20 +14,43 @@ namespace ClassHub.Server.Controllers {
         const string database = "classdb";
         const string connectionString = $"Host={host};Username={username};Password={passwd};Database={database}";
 
-        // 해당 강의실에 올라온 시험 리스트를 보여준다.(교수용)
-        [HttpGet("professor/room_id/{room_id}")]
-        public List<Submission> GetExamList(int room_id) {
+        // 해당 강의실에 올라온 과제 리스트를 보여준다.(학생용)
+        [HttpGet("student/room_id/{room_id}/student_id/{student_id}")]
+        public List<Submission> GetPracticeList(int room_id, int student_id) {
             using var connection = new NpgsqlConnection(connectionString);
-            string query;
 
             // 시험 리스트
             List<Submission> examList = new List<Submission>();
 
             // 강의실 번호가 room_id인 실습들을 찾습니다.
-            query = "SELECT * FROM exam WHERE room_id = @room_id;";
+            string query = "SELECT * FROM exam WHERE room_id = @room_id;";
             var parameters = new DynamicParameters();
             parameters.Add("room_id", room_id);
             var exams = connection.Query<Shared.Exam>(query, parameters);
+
+            // TODO : ExamSubmit이 정립되면 Exam 객체를 생성할때 해당 학생의 제출여부 수정 필요
+
+            foreach (Shared.Exam exam in exams) {
+                examList.Add(new Client.Models.Exam { Id = exam.exam_id, Title = exam.title, Author = exam.author, StartDate = exam.start_date, EndDate = exam.end_date, IsSubmitted = false, TotalSubmitters = 0 });
+            }
+            return examList;
+        }
+
+        // 해당 강의실에 올라온 시험 리스트를 보여준다.(교수용)
+        [HttpGet("professor/room_id/{room_id}")]
+        public List<Submission> GetExamList(int room_id) {
+            using var connection = new NpgsqlConnection(connectionString);
+
+            // 시험 리스트
+            List<Submission> examList = new List<Submission>();
+
+            // 강의실 번호가 room_id인 실습들을 찾습니다.
+            string query = "SELECT * FROM exam WHERE room_id = @room_id;";
+            var parameters = new DynamicParameters();
+            parameters.Add("room_id", room_id);
+            var exams = connection.Query<Shared.Exam>(query, parameters);
+
+            // TODO : ExamSubmit이 정립되면 제출한 학생이 몇명인지에 대한 정보를 Exam 객체를 생성할 때 포함해야함
 
             foreach (Shared.Exam exam in exams) {
                 examList.Add(new Client.Models.Exam { Id = exam.exam_id, Title = exam.title, Author = exam.author, StartDate = exam.start_date, EndDate = exam.end_date, IsSubmitted = false, TotalSubmitters = 0 });
