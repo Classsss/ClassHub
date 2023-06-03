fbaa5a2fc608029c46e0734938c19f2f4090e81b
LeeHaYoon <htdobad@gmail.com>
2023-06-04 03:03:03 +0900

    db 연동을 위한 모델 클래스 추가

---
+++ Shared/ExamSubmit.cs
@@ -0,0 +1,11 @@
+﻿namespace ClassHub.Shared {
+    public class ExamSubmit {
+        public int submit_id { get; set; }
+        public int exam_id { get; set; }
+	    public int room_id { get; set; }
+        public int student_id { get; set; }
+	    public string student_name { get; set; }
+        public int score { get; set; }
+	    public DateTime submit_date { get; set; }
+    }
+}
---
+++ Shared/ExamSubmitContainer.cs
@@ -0,0 +1,16 @@
+﻿namespace ClassHub.Shared {
+    // ExamSubmit 클래스와 시험 문제 제출 클래스들을 함께 저장하는 클래스
+    public class ExamSubmitContainer {
+        // 시험 제출
+        public ExamSubmit? ExamSubmit { get; set; } = null;
+
+        // 객관식 문제 제출 리스트
+        public List<MultipleChoiceProblemSubmit> MultipleChoiceProblemSubmits { get; set; } = new List<MultipleChoiceProblemSubmit>();
+
+        // 단답형 문제 제출 리스트
+        public List<ShortAnswerProblemSubmit> ShortAnswerProblemSubmits { get; set; } = new List<ShortAnswerProblemSubmit>();
+
+        // 코드형 문제 제출 리스트
+        public List<CodingExamProblemSubmit> CodingExamProblemSubmits { get; set; } = new List<CodingExamProblemSubmit>();
+    }
+}
