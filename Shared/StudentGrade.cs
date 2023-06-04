1c82c9a095e3e793c3a35e5f320addc494fca68b
byungmeo <wlsrlfvkr@gmail.com>
2023-06-04 14:09:39 +0900

    학생들의 성적을 CSV 파일로 출력하는 버튼 및 기능 추가

--- Shared/StudentGrade.cs
+++ Shared/StudentGrade.cs
@@ -1,4 +1,7 @@
-﻿namespace ClassHub.Shared {
+﻿using CsvHelper.Configuration;
+using System.Text.Json.Serialization;
+
+namespace ClassHub.Shared {
     public class StudentGrade {
         private GradeRatio grade_ratio;
         private double _attendance_score;
@@ -34,6 +37,22 @@
         }
         public double final_score {
             get { return attendance_score + assignment_score + practice_score + exam_score; }
+        }
+
+        [JsonIgnore]
+        public string FullName {
+            get { return $"{name}({student_id})"; }
+        }
+    }
+
+    public class StudentGradeMap : ClassMap<StudentGrade> {
+        public StudentGradeMap() {
+            Map(m => m.FullName).Name("학생(학번)");
+            Map(m => m.attendance_score).Name("출석");
+            Map(m => m.assignment_score).Name("과제");
+            Map(m => m.practice_score).Name("실습");
+            Map(m => m.exam_score).Name("시험");
+            Map(m => m.final_score).Name("최종");
         }
     }
 }
